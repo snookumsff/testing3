07af3f7a4463f02b8b88fd3b69c501146b8d4589
annaltsh <annaltsh>
2025-09-19 06:13:22 +0300

    print-esp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,9 @@
 using namespace std;
 int main()
 {
-    cout << "Hello, world";
-    cout << "Привет, мир";
-    cout << "Bonjour le mode";
+    cout << "Hello, world!";
+    cout << "Привет, мир!";
+    cout << "Bonjour le mode!";
+    cout << "Hola Mundo!";
     return 0;
 }
